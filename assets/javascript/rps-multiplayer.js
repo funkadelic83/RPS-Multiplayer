ddad5505dcec8dd29d43b0be6a719d51f8c1d54b
funkadelic83 <mikedamanskis@gmail.com>
2019-03-25 15:27:25 -0700

    initial commit

---
+++ assets/javascript/rps-multiplayer.js
@@ -0,0 +1,12 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD837J3WCp3T_t9drbsQ3zpLi23NHxvNFk",
+    authDomain: "rps-multiplayer-1e814.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-1e814.firebaseio.com",
+    projectId: "rps-multiplayer-1e814",
+    storageBucket: "",
+    messagingSenderId: "457074219036"
+  };
+
+  firebase.initializeApp(config);
