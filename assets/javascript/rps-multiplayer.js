f096bbbb1e9a91e3bd8865917e4fe73434d8e97f
funkadelic83 <mikedamanskis@gmail.com>
2019-03-29 13:45:15 -0700

    improve javascript to show the winners, but still need to fix the database update and reset game

--- assets/javascript/rps-multiplayer.js
+++ assets/javascript/rps-multiplayer.js
@@ -22,6 +22,8 @@ window.onload = function () {
   var playerTwoActive = false;
   var playerOneNameLocal = "";
   var playerTwoNameLocal = "";
+  var playerOneWins = 0;
+  var playerTwoWins = 0;
   $("#login").html("<input type='text' id='playerOneNameInput'><button id='playerOneSubmit'>Player One Name</button>");
   $("status").html("Waiting on Players");
   $("#login2").html("<input type='text' id='playerTwoNameInput'><button id='playerTwoSubmit'>Player Two Name</button>");
@@ -35,8 +37,7 @@ window.onload = function () {
     database.ref("playerOne").set({
       name: playerOneNameLocal,
       active: true,
-      wins: 0,
-      losses: 0
+      wins: playerOneWins,
     }).then(function () {
       var player;
       database.ref("playerOne").on('value', function (snapshot) {
@@ -57,8 +58,7 @@ window.onload = function () {
     database.ref("playerTwo").set({
       name: playerTwoNameLocal,
       active: true,
-      wins: 0,
-      losses: 0
+      wins: playerTwoWins,
     }).then(function () {
       var player2;
       firebase.database().ref("playerTwo").on('value', function (snapshot) {
@@ -101,117 +101,190 @@ window.onload = function () {
 
     if (snapshot.child("playerOne").val().active === true && snapshot.child("playerTwo").val().active === true) {
       // console.log("Holy heck! Both players are active!");
-      beginGame();
+      $("#playerOneChoices").html("<button id='p1Rock'>Rock</button><br><button id='p1Paper'>Paper</button><br><button id='p1Scizzors'>Scizzors</button><br>");
+      $("#playerTwoChoices").html("<button id='p2Rock'>Rock</button><br><button id='p2Paper'>Paper</button><br><button id='p2Scizzors'>Scizzors</button><br>");
+
+      var playerOneChoice;
+      var playerTwoChoice;
+      // var playerOneChosen = false; <-- i don't think i need these, since the variables above truthy
+      // var playerTwoChosen = false;
+
+      //PLAYER ONE CHOOSES THEIR WEAPON, SETS THE WEAPON IN THE DATABASE
+
+      $("#p1Rock").on("click", function () {
+        playerOneChoice = "rock";
+        database.ref("/playerOne").update({
+          weapon: playerOneChoice
+        }).then(function () {
+          database.ref().on("value", function (snapshot) {
+            $("#playerOneChoices").html("Player One has chosen " + snapshot.child("playerOne").val().weapon);
+          });
+        }).catch(function (error) {
+          if (error) throw error;
+        })
+      })
+
+      $("#p1Scizzors").on("click", function () {
+        playerOneChoice = "scizzors";
+        database.ref("/playerOne").update({
+          weapon: playerOneChoice
+        }).then(function () {
+          database.ref().on("value", function (snapshot) {
+            $("#playerOneChoices").html("Player One has chosen " + snapshot.child("playerOne").val().weapon);
+          });
+        }).catch(function (error) {
+          if (error) throw error;
+        })
+      })
+
+      $("#p1Paper").on("click", function () {
+        playerOneChoice = "paper";
+        database.ref("/playerOne").update({
+          weapon: playerOneChoice
+        }).then(function () {
+          database.ref().on("value", function (snapshot) {
+            $("#playerOneChoices").html("Player One has chosen " + snapshot.child("playerOne").val().weapon);
+          });
+        }).catch(function (error) {
+          if (error) throw error;
+        })
+      })
+
+      //PLAYER TWO CHOOSES THEIR WEAPON, SETS THE WEAPON IN THE DATABASE
+
+      $("#p2Rock").on("click", function () {
+        playerTwoChoice = "rock";
+        database.ref("/playerTwo").update({
+          weapon: playerTwoChoice
+        }).then(function () {
+          database.ref().on("value", function (snapshot) {
+            $("#playerTwoChoices").html("Player Two has chosen " + snapshot.child("playerTwo").val().weapon);
+          });
+        }).catch(function (error) {
+          if (error) throw error;
+        })
+      })
+
+      $("#p2Scizzors").on("click", function () {
+        playerTwoChoice = "scizzors";
+        database.ref("/playerTwo").update({
+          weapon: playerTwoChoice
+        }).then(function () {
+          database.ref().on("value", function (snapshot) {
+            $("#playerTwoChoices").html("Player Two has chosen " + snapshot.child("playerTwo").val().weapon);
+          });
+        }).catch(function (error) {
+          if (error) throw error;
+        })
+      })
+
+      $("#p2Paper").on("click", function () {
+        playerTwoChoice = "paper";
+        database.ref("/playerTwo").update({
+          weapon: playerTwoChoice
+        }).then(function () {
+          database.ref().on("value", function (snapshot) {
+            $("#playerTwoChoices").html("Player Two has chosen " + snapshot.child("playerTwo").val().weapon);
+          });
+        }).catch(function (error) {
+          if (error) throw error;
+        })
+      })
+
+      //DETERMINE IF BOTH PLAYERS HAVE CHOSEN A WEAPON
+      if (snapshot.child("playerOne").val().weapon && snapshot.child("playerTwo").val().weapon) {
+
+
+        //SINCE BOTH PLAYERS HAVE CHOSEN A WEAPON, WE CAN DETERMINE THE WINNER
+
+        //IF THE GAME IS A TIE
+        if (snapshot.child("playerOne").val().weapon === snapshot.child("playerTwo").val().weapon) {
+          $("#winner").html("It's a damn tie!");
+
+          //IF PLAYER ONE CHOOSES ROCK
+        } else if (snapshot.child("playerOne").val().weapon === "rock") {
+          if (snapshot.child("playerTwo").val().weapon === "paper") {
+            playerOneWins++;
+            var weaponReset = null;
+            database.ref("/playerOne").update({
+              wins: playerOneWins,
+              weapon: weaponReset
+
+
+////THIS IS WHERE I LEFT OFF - RIGHT ABOVE THIS LINE. WHEN THE WEAPON IS TAKEN AWAY, THE USER CHOOSES DISPLAYS AS UNDEFINED.
+
+
+            }).then(function () {
+              database.ref().on("value", function (snapshot) {
+                $("#p1Wins").html("Player One has won " + snapshot.child("playerOne").val().wins);
+                $("#winner").html("Paper Covers rock. Player 2 wins");
+              });
+            }).catch(function (error) {
+              if (error) throw error;
+            })
+        
+            
+
+
+
+
+          } else if (snapshot.child("playerTwo").val().weapon === "scizzors") {
+            $("#winner").html("Rock smashes scizzors. Player 1 wins.");
+
+
+
+
+          } else {
+            $("#winner").html("Error!");
+          }
+
+          //IF PLAYER ONE CHOOSES SCIZZORS
+        } else if (snapshot.child("playerOne").val().weapon === "paper") {
+          if (snapshot.child("playerTwo").val().weapon === "scizzors") {
+            $("#winner").html("Scizzors cut paper. Player 2 wins!");
+          } else if (snapshot.child("playerTwo").val().weapon === "rock") {
+            $("#winner").html("Paper covers rock. Player 1 wins!");
+          } else {
+            $("#winner").html("Error!");
+          }
+
+          //IF PLAYER ONE CHOOSES PAPER
+
+        } else if (snapshot.child("playerOne").val().weapon === "paper") {
+          if (snapshot.child("playerTwo").val().weapon === "scizzors") {
+            $("#winner").html("Scizzors cut paper. Player two wins!");
+          } else if (snapshot.child("playerTwo").val().weapon === "rock") {
+            $("#winner").html("Paper covers rock. Player 1 wins!");
+          } else {
+            $("#winner").html("Error!");
+          }
+        }
+
+
+
+
+
+      }
+      // })
+
+
+
+
+
     }
   })
 
-  function beginGame() {
-
-    $("#playerOneChoices").html("<button id='p1Rock'>Rock</button><br><button id='p1Paper'>Paper</button><br><button id='p1Scizzors'>Scizzors</button><br>");
-    $("#playerTwoChoices").html("<button id='p2Rock'>Rock</button><br><button id='p2Paper'>Paper</button><br><button id='p2Scizzors'>Scizzors</button><br>");
-
-    var playerOneChoice;
-    var playerTwoChoice;
-    // var playerOneChosen = false; <-- i don't think i need these, since the variables above truthy
-    // var playerTwoChosen = false;
-
-    //PLAYER ONE CHOOSES THEIR WEAPON, SETS THE WEAPON IN THE DATABASE
-
-    $("#p1Rock").on("click", function () {
-      playerOneChoice = "rock";
-      database.ref("/playerOne").update({
-        weapon: playerOneChoice
-      }).then(function () {
-        database.ref().on("value", function (snapshot) {
-          $("#playerOneChoices").html("Player One has chosen " + snapshot.child("playerOne").val().weapon);
-        });
-      }).catch(function (error) {
-        if (error) throw error;
-      })
-    })
-
-    $("#p1Scizzors").on("click", function () {
-      playerOneChoice = "scizzors";
-      database.ref("/playerOne").update({
-        weapon: playerOneChoice
-      }).then(function () {
-        database.ref().on("value", function (snapshot) {
-          $("#playerOneChoices").html("Player One has chosen " + snapshot.child("playerOne").val().weapon);
-        });
-      }).catch(function (error) {
-        if (error) throw error;
-      })
-    })
-
-    $("#p1Paper").on("click", function () {
-      playerOneChoice = "paper";
-      database.ref("/playerOne").update({
-        weapon: playerOneChoice
-      }).then(function () {
-        database.ref().on("value", function (snapshot) {
-          $("#playerOneChoices").html("Player One has chosen " + snapshot.child("playerOne").val().weapon);
-        });
-      }).catch(function (error) {
-        if (error) throw error;
-      })
-    })
-
-    //PLAYER TWO CHOOSES THEIR WEAPON, SETS THE WEAPON IN THE DATABASE
-
-    $("#p2Rock").on("click", function () {
-      playerTwoChoice = "rock";
-      database.ref("/playerTwo").update({
-        weapon: playerTwoChoice
-      }).then(function () {
-        database.ref().on("value", function (snapshot) {
-          $("#playerTwoChoices").html("Player Two has chosen " + snapshot.child("playerTwo").val().weapon);
-        });
-      }).catch(function (error) {
-        if (error) throw error;
-      })
-    })
-
-    $("#p2Scizzors").on("click", function () {
-      playerTwoChoice = "scizzors";
-      database.ref("/playerTwo").update({
-        weapon: playerTwoChoice
-      }).then(function () {
-        database.ref().on("value", function (snapshot) {
-          $("#playerTwoChoices").html("Player Two has chosen " + snapshot.child("playerTwo").val().weapon);
-        });
-      }).catch(function (error) {
-        if (error) throw error;
-      })
-    })
-
-    $("#p2Paper").on("click", function () {
-      playerTwoChoice = "paper";
-      database.ref("/playerTwo").update({
-        weapon: playerTwoChoice
-      }).then(function () {
-        database.ref().on("value", function (snapshot) {
-          $("#playerTwoChoices").html("Player Two has chosen " + snapshot.child("playerTwo").val().weapon);
-        });
-      }).catch(function (error) {
-        if (error) throw error;
-      })
-    })
-
-// database.ref().on("value", function (snapshot) {
-//     if (snapshot.child("playerOne").val().weapon) && snapshot.child("playerTwo").val().weapon)) {
-//       console.log("Both are truthy!");
-//     }
-//   })
-//   }
-  //IF BOTH PLAYERS ARE ACTIVE, THE BATTLE OPTIONS DISPLAY. THIS NEEDS TO BE CALLED WHEN BOTH PLAYERES ARE ACTIVE. SINCE I HAVEN'T SOLVED HOW TO CHECK THE DATABASE TO CHECK IF BOTH PLAYERS ARE ACTIVE, I HAVE IT DISPLAY BY DEFAULT.
-  //--------------------------------------------------
 
 
 
 
-  //DETERMINE IF EACH PLAYER HAS CHOSEN A WEAPON (THIS NEEDS TO BE DONE IN THE DATABASE, BUT ITS STORED WITH LOCAL VARIABLES FOR NOW)
 
-  // function choiceCheck() {
+  //END BEGIN GAME
+  //ADD CHAT FUNCTIONALITY. I WILL BE ABLE TO DO THIS EASIER ONCE I FULLY GRASP THE STEPS I'M MISSING
+}// END ONWINDOWLOAD
+
+
+ // function choiceCheck() {
   //   if (playerOneChosen === false && playerTwoChosen === false) {
   //     $("#winner").html("Waiting on Both Players to Choose Their Weapons");
   //   } else if (playerOneChosen === true && playerTwoChosen === false) {
@@ -277,12 +350,6 @@ window.onload = function () {
 
 
   // }
-
-  //ADD CHAT FUNCTIONALITY. I WILL BE ABLE TO DO THIS EASIER ONCE I FULLY GRASP THE STEPS I'M MISSING
-}
-
-
-
 
 
 
